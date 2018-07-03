c2a8be43019549370e65bbd67f3ff115acdc4282
Jinesh Patel <jinesh701@gmail.com>
2018-07-03 01:02:38 -0400

    changed order of code

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ let app = express();
 
 app.use(express.static('public'));
 
+let server = app.listen(8080, () => {
+  console.log('listening on port 8080');
+});
+
 //Socket setup
 let io = socket(server);
 
 io.on('connection', socket => {
-  console.log('made socket connection');
-});
-
-let server = app.listen(8080, () => {
-  console.log('listening on port 8080');
+  console.log('made socket connection', socket.id);
 });
